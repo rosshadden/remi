1797e974e39a1626a89bdcb8695181448134659c
Ross Hadden <rosshadden@gmail.com>
2013-09-19 09:38:42 -0400

    Added header with (static) breadcrumbs and a home page.

--- controllers/database.js
+++ controllers/database.js
@@ -1,6 +1,6 @@
 module.exports = {
 	index(req, res) {
-		res.redirect("list")
+		res.redirect("/database/list")
 	},
 
 	list(req, res) {
--- routes.js
+++ routes.js
@@ -2,7 +2,7 @@ var services = app.util.loader.dirSync("services");
 
 var routes = {
 	"/": (req, res) => {
-		res.redirect("/database/list");
+		res.view("index");
 	}
 };
 
